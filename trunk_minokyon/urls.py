53a8e2df74d420ae11cafe88d4db1dbdc6539b21
kyosuke <kyosuke.araki611@gmail.com>
2018-03-08 19:10:52 +0900

    Makemigrations

--- trunk_minokyon/urls.py
+++ trunk_minokyon/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^log', include('login.urls', namespace='login')),
     url(r'^minono/', include('minono.urls', namespace='minono')),
+    url(r'', include('social_django.urls', namespace='social')),
 ]
