0485bbaefb56415e22f146dcabffbba7f198be45
kyosuke <kyosuke.araki611@gmail.com>
2018-03-09 00:16:15 +0900

    Add static url

--- trunk_minokyon/urls.py
+++ trunk_minokyon/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -5,4 +7,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^minono/', include('minono.urls', namespace='minono')),
     url(r'', include('index.urls', namespace='index')),
-]
+] + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)  # 静的ファイルへのアクセスURL
